57a648dbb5a82159546a554d067e51aea6fd8478
Igor <igorrybalko2009@gmail.com>
2025-10-26 22:08:24 +0200

    added host banner

--- includes/common/count.php
+++ includes/common/count.php
@@ -3,7 +3,7 @@ $count = wp_count_posts('directory-item');
 $onModeration = getCountFormPending();
 ?>
 
-<div class="border-[3px] border-blue-400 rounded-md pb-3 pl-2 pr-2 pt-1">
+<div class="border-[3px] border-blue-400 rounded-md pb-3 pl-2 pr-2 pt-1 mb-6">
     <h3 class="font-bold mb-4 text-lg">Статистика</h3>
     <div class="text-sm">
         <div class="mb-2">
--- includes/common/sidebar.php
+++ includes/common/sidebar.php
@@ -3,4 +3,7 @@
 require_once (__DIR__ . '/new_sites.php');
 require_once (__DIR__ . '/count.php');
 ?>
+<a target="_blank" href="https://www.ukraine.com.ua/?page=561268" title="Хостинг Україна" rel="nofollow">
+    <img class="rounded-md" src="<?php echo get_template_directory_uri(); ?>/img/hostukr.jpg" alt="Hosting Ukraine"/>
+</a>
 </aside>
