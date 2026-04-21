5ac8e0caef4f04cdef9113a5395624886f22e645
Igor <igorrybalko2009@gmail.com>
2026-04-21 20:11:28 +0300

    added wslogo

--- footer.php
+++ footer.php
@@ -9,32 +9,38 @@
                             </span>
                 </div>
               
-                <div class="fmenuwr">
-                    <nav class="foot-menu">
+                <div>
+                    <nav class="text-sm">
                         <?php wp_nav_menu( array('theme_location'  => 'bottom')); ?>
                     </nav>
                 </div>
 
                 <div class="slinkwr">
-            
-                    <a href='http://hit.ua/?x=15878' target='_blank'>
-                        <script>
-                        Cd=document;Cr="&"+Math.random();Cp="&s=1";
-                        Cd.cookie="b=b";if(Cd.cookie)Cp+="&c=1";
-                        Cp+="&t="+(new Date()).getTimezoneOffset();
-                        if(self!=top)Cp+="&f=1";
-                        
-                        Cd.write("<img src='//c.hit.ua/hit?i=15878&g=0&x=4"+Cp+Cr+
-                        "&r="+escape(Cd.referrer)+"&u="+escape(window.location.href)+
-                        "' border='0' width='88' height='15' "+
-                        "alt='hitua' />");
-                        </script>
-                    </a>
-                 
-                </div>
-              
-            </div>
 
+                    <div class="mb-3">
+                        <a href='http://hit.ua/?x=15878' target='_blank'>
+                            <script>
+                            Cd=document;Cr="&"+Math.random();Cp="&s=1";
+                            Cd.cookie="b=b";if(Cd.cookie)Cp+="&c=1";
+                            Cp+="&t="+(new Date()).getTimezoneOffset();
+                            if(self!=top)Cp+="&f=1";
+                            
+                            Cd.write("<img src='//c.hit.ua/hit?i=15878&g=0&x=4"+Cp+Cr+
+                            "&r="+escape(Cd.referrer)+"&u="+escape(window.location.href)+
+                            "' border='0' width='88' height='15' "+
+                            "alt='hitua' />");
+                            </script>
+                        </a>
+                    </div>
+
+                    <div class="w-10">
+                        <a href="https://webstep.top/ua/" target="_blank" title="Розробка і підтримка сайту">
+                            <img src="<?php echo get_template_directory_uri(); ?>/img/footprint.svg" alt="webstep">
+                        </a>
+                    </div>
+            
+                </div>
+            </div>
         </div>
     
 </footer>
